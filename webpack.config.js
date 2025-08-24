3b92e85e25851c5d0d5422c146bb190a1517ce7e
IKE1213 <ikedakoukun1213@gmail.com>
2025-08-25 01:36:28 +0900

    webpack.config.jsの設定(モジュールのルール付けまで完成)

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: {
+        bundle: "./src/index.ts",
+    },
+    output: {
+        path: `${__dirname}/dist`,
+        filename: "bundle.js",
+    },
+    mode: "development",
+    resolve: {
+        extension: [".ts",".js"],
+    },
+    devServer:{
+        static: {
+            directory: `${__dirname}/dist`,
+        },
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+            test: /\.ts$/,
+            loader: "ts-loader",
+            },
+        ],
+    },
+};
